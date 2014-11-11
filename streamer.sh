d62a48f874a1e6d2b9284b95c889b70d6623b422
Patrick Carey <patrick@rehabstudio.com>
2014-11-11 23:08:20 +0000

    removed redundant bash script

--- streamer.sh
+++ streamer.sh
@@ -5,8 +5,16 @@ set -e
 # background process that just sends dweets over and over again until told to
 # stop.
 
+# pass key in the url if 2nd cli arg is passed to script
+if [ -z "$2" ]
+    then
+        key=""
+    else
+        key="?key=$2"
+fi
+
 for i in {1..10}; do
-    curl -s -o output.txt -H "Content-Type: application/json" -d '{"hello":"world","somenum":6816513845}' https://dweet.io/dweet/for/$1
+    curl -s -o output.txt -H "Content-Type: application/json" -d '{"hello":"world","somenum":6816513845}' https://dweet.io/dweet/for/$1$key
     rm output.txt
     sleep 1
 done
